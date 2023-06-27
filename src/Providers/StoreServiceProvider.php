e35d6906f25c31db1f126db0dc69ab3c708ea4b6
Maksym <m.guzichko@gmail.com>
2023-06-27 22:21:57 +0200

    add service provider

---
+++ src/Providers/StoreServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Maxguzichko\Larastore\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class StoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
