bae55356a01c1eddba5c9c29c57093ff124b3fde
Maksym <m.guzichko@gmail.com>
2023-07-15 22:17:22 +0200

    goods table && creating images folder

--- src/Providers/StoreServiceProvider.php
+++ src/Providers/StoreServiceProvider.php
@@ -19,6 +19,8 @@ class StoreServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'larastore');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
